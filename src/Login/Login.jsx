451f909ab2a7faef0509629d8653a003297e06ff
AB-ohi <askohi63907@gmail.com>
2024-01-06 12:46:01 +0600

    90% login page complite

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -8,22 +8,71 @@ const Login = () => {
           <img className="login-img" src="../../public/img/login.png" alt="" />
         </div>
         <div className="login-input">
-          <div style={{marginBottom:"32px"}}>
+          <div style={{ marginBottom: "32px" }}>
             <h1 className="login-header">Welcome</h1>
             <p style={{ color: "rgb(168, 168, 168)", margin: "0" }}>
               Please login here
             </p>
           </div>
-          <form>
-            <div style={{display:'flex', flexDirection:'column'}}>
-            <label htmlFor="email">Email Address</label>
-            <input type="email" name="email" placeholder="email" id="" />
+          <form style={{ width: "60%" }}>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <label htmlFor="email">Email Address</label>
+              <input
+                style={{
+                  padding: "16px",
+                  border: "1px solid #131118",
+                  outline: "none",
+                  borderRadius: "10px",
+                  marginBottom:'18px'
+                }}
+                className="import-email"
+                type="email"
+                name="email"
+                placeholder="email"
+                id=""
+              />
             </div>
-            <div style={{display:'flex', flexDirection:'column'}}>
+            <div style={{ display: "flex", flexDirection: "column" }}>
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="Password" id="" />
+              <input
+                style={{
+                  padding: "16px",
+                  border: "1px solid #131118",
+                  outline: "none",
+                  borderRadius: "10px",
+                }}
+                type="password"
+                name="password"
+                placeholder="Password"
+                id=""
+              />
             </div>
-            <input type="submit" value="Login" />
+            <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+              <div style={{display:'flex'}}>
+                <input
+                  type="checkbox"
+                  name="remember"
+                  id=""
+                  style={{ background: "#131118" }}
+                />
+                <p>Remember Me</p>
+              </div>
+              <div>
+              <a href="">Forget Password?</a>
+              </div>
+            </div>
+            <input
+              style={{
+                width: "100%",
+                background: "#131118",
+                color: "rgba(255, 255, 255, 1)",
+                border: "none",
+                padding: "20px 0",
+                borderRadius: "10px",
+              }}
+              type="submit"
+              value="Login"
+            />
           </form>
         </div>
       </div>
