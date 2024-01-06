efe582d1749897c8f407e87608f474b43024897f
AB-ohi <askohi63907@gmail.com>
2024-01-07 00:56:19 +0600

    added

---
+++ src/Firebase/firebase.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA588cXIFi7QYI5BggExHsiU6e9m0GgvNw",
+  authDomain: "krist-shop-59ff2.firebaseapp.com",
+  projectId: "krist-shop-59ff2",
+  storageBucket: "krist-shop-59ff2.appspot.com",
+  messagingSenderId: "1015240012069",
+  appId: "1:1015240012069:web:ac780852ac85f2908aaacc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
