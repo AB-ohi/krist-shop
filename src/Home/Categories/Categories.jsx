8bde282311e985610f8efcbc5596e437c526260f
AB-ohi <askohi63907@gmail.com>
2024-01-11 01:35:36 +0600

    added

--- src/Home/Categories/Categories.jsx
+++ src/Home/Categories/Categories.jsx
@@ -1,7 +1,82 @@
+import Carousel from "react-multi-carousel";
+
 const Categories = () => {
     return (
         <div>
-            
+            <Carousel
+  additionalTransfrom={0}
+  arrows
+  autoPlay
+  autoPlaySpeed={1000}
+  centerMode={false}
+  className=""
+  containerClass="container-with-dots"
+  dotListClass=""
+  draggable
+  focusOnSelect={false}
+  infinite
+  itemClass=""
+  keyBoardControl
+  minimumTouchDrag={80}
+  pauseOnHover
+  renderArrowsWhenDisabled={false}
+  renderButtonGroupOutside={false}
+  renderDotsOutside={false}
+  responsive={{
+    desktop: {
+      breakpoint: {
+        max: 3000,
+        min: 1024
+      },
+      items: 3,
+      partialVisibilityGutter: 40
+    },
+    mobile: {
+      breakpoint: {
+        max: 464,
+        min: 0
+      },
+      items: 1,
+      partialVisibilityGutter: 30
+    },
+    tablet: {
+      breakpoint: {
+        max: 1024,
+        min: 464
+      },
+      items: 2,
+      partialVisibilityGutter: 30
+    }
+  }}
+  rewind={false}
+  rewindWithAnimation={false}
+  rtl={false}
+  shouldResetAutoplay
+  showDots={false}
+  sliderClass=""
+  slidesToSlide={2}
+  swipeable
+>
+    {/* eslint-disable-next-line react/jsx-no-undef */}
+  <WithStyles
+    description="Fixing CSS load order/style.chunk.css incorrect in Nextjs"
+    headline="w3js.com - web front-end studio"
+    image="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  />
+    {/* eslint-disable-next-line react/jsx-no-undef */}
+  <WithStyles
+    description="React Carousel with Server Side Rendering Support – Part 2"
+    headline="w3js.com - web front-end studio"
+    image="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+  />
+  {/* eslint-disable-next-line react/jsx-no-undef */}
+  <WithStyles
+    description="React Carousel with Server Side Rendering Support – Part 1"
+    headline="w3js.com - web front-end studio"
+    image="https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  />
+  
+</Carousel>
         </div>
     );
 };
