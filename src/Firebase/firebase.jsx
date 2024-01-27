dee44275be468d14f70f450baac6b68f794d1bb6
AB-ohi <askohi63907@gmail.com>
2024-01-27 20:04:55 +0600

    added

--- src/Firebase/firebase.jsx
+++ src/Firebase/firebase.jsx
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  // apiKey: import.meta.env.VITE_apiKey,
+  // authDomain: import.meta.env.VITE_authDomain,
+  // projectId: import.meta.env.VITE_projectId,
+  // storageBucket: import.meta.env.VITE_storageBucket,
+  // messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  // appId: import.meta.env.VITE_appId
+  apiKey: "AIzaSyA588cXIFi7QYI5BggExHsiU6e9m0GgvNw",
+  authDomain: "krist-shop-59ff2.firebaseapp.com",
+  projectId: "krist-shop-59ff2",
+  storageBucket: "krist-shop-59ff2.appspot.com",
+  messagingSenderId: "1015240012069",
+  appId: "1:1015240012069:web:ac780852ac85f2908aaacc"
 
   
 };
