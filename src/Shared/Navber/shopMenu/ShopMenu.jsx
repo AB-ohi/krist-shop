4dd5ec184f24f9341279e187e284e52b496ab7c4
ABohi <askohi63907@gmail.com>
2024-02-03 22:58:42 -0800

    added

--- src/Shared/Navber/shopMenu/ShopMenu.jsx
+++ src/Shared/Navber/shopMenu/ShopMenu.jsx
@@ -1,55 +1,56 @@
+import './ShopMenu.css'
 const ShopMenu = () => {
     return (
         <table>
-            <tr>
+            <tr className="table-row">
                 <th>Men</th>
                 <th>Women</th>
                 <th>Footwear</th>
                 <th>Kids</th>
             </tr>
-            <tr>
+            <tr className="table-row">
                 <td>T-Shirt</td>
                 <td>Kurtas & Suits</td>
                 <td>Flats</td>
                 <td>T-Shirt</td>
             </tr>
-            <tr>
+            <tr className="table-row">
                 <td>Casusl Shirts</td>
                 <td>Sarees</td>
                 <td>Casual Shoes</td>
                 <td>Shirts</td>
             </tr>
-            <tr>
+            <tr className="table-row">
                 <td>Formal Shirts</td>
                 <td>Ethnic Wear</td>
                 <td>Heels</td>
                 <td>Jeans</td>
             </tr>
-            <tr>
+            <tr className="table-row">
                 <td>Jackets</td>
                 <td>Lehenga Cholis</td>
                 <td>Boots</td>
                 <td>Trousers</td>
             </tr>
-            <tr>
+            <tr className="table-row">
                 <td>Blazers & Coats</td>
                 <td>Jackets</td>
                 <td>Sports Shoes & Floaters</td>
                 <td>Party wear</td>
             </tr>
-            <tr>
+            <tr className="table-row">
                 <th>Indian & Festive Wear</th>
                 <th>Western Wear</th>
                 <th>Product Features</th>
                 <td>Innerwear & Thermal</td>
             </tr>
-            <tr>
+            <tr className="table-row">
                 <td>Kurta & Kurta Set</td>
                 <td>Dresses</td>
                 <td>360 Product Viewer</td>
                 <td>Track pants</td>
             </tr>
-            <tr>
+            <tr className="table-row">
                 <td>Sherwanis</td>
                 <td>Jumpsuits</td>
                 <td>Product with Video</td>
