fa5c69d2772e314bea9ebde3fb799f8553e6ab87
ABohi <askohi63907@gmail.com>
2024-02-08 11:23:29 -0800

    added

--- src/Home/Bestseller/Bestseller.jsx
+++ src/Home/Bestseller/Bestseller.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import './BestSeller.css'
+import { useEffect } from 'react';
 
 const Bestseller = () => {
+    const [menCollections, SetMenCollections] = useState();
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/men")
+        .then(res => res.json())
+        .then(data =>SetMenCollections(data))
+    },[])
     return (
         <div>
             <h1 style={{textAlign:'center'}}>Our Bestseller</h1>
             <div className='best-seller-card-body'>
                 {
-                    
+                    menCollections.map(menCollection=>
+                    <div key={menCollection._id}>
+
+                    </div>)
                 }
             </div>
         </div>
