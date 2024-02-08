087e9257609b6786563b24129629901fda1d9cb4
ABohi <askohi63907@gmail.com>
2024-02-08 11:43:33 -0800

    added

--- src/Home/Bestseller/Bestseller.jsx
+++ src/Home/Bestseller/Bestseller.jsx
@@ -1,28 +1,37 @@
-import { useState } from 'react';
-import './BestSeller.css'
-import { useEffect } from 'react';
+import { useState } from "react";
+import "./BestSeller.css";
+import { useEffect } from "react";
 
 const Bestseller = () => {
-    const [menCollections, SetMenCollections] = useState([]);
+  const [menCollections, SetMenCollections] = useState([]);
 
-    useEffect(()=>{
-        fetch("http://localhost:5000/men")
-        .then(res => res.json())
-        .then(data =>SetMenCollections(data))
-    },[])
-    return (
-        <div>
-            <h1 style={{textAlign:'center'}}>Our Bestseller</h1>
-            <div className='best-seller-card-body'>
-                {
-                    menCollections.map(menCollection=>
-                    <div key={menCollection._id}>
-                        <img style={{width:'100%', border:'1px solid red'}} src={menCollection.pictureURL} alt="" />
-                    </div>)
-                }
+  useEffect(() => {
+    fetch("http://localhost:5000/men")
+      .then((res) => res.json())
+      .then((data) => SetMenCollections(data));
+  }, []);
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Our Bestseller</h1>
+      <div className="best-seller-card-body">
+        {menCollections.map((menCollection) => (
+          <div style={{ border: "1px solid red" }} key={menCollection._id}>
+            <img
+              style={{ width: "100%" }}
+              src={menCollection.pictureURL}
+              alt=""
+            />
+            <h1 style={{ margin: "0" }}>{menCollection.productName}</h1>
+            <p style={{ margin: "0" }}>{menCollection.nickname}</p>
+            <div>
+              <p></p>
+              <p></p>
             </div>
-        </div>
-    )
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Bestseller;
