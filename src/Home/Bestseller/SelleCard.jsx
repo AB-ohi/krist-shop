72ac76cf3d61751009e834455a1afe5894a1b6ce
AB-ohi <askohi63907@gmail.com>
2024-02-12 18:57:14 +0600

    added

--- src/Home/Bestseller/SelleCard.jsx
+++ src/Home/Bestseller/SelleCard.jsx
@@ -7,7 +7,17 @@ const SelleCard = ({pictureURL,productName,nickname,price}) => {
 
     return (
         <div>
-           
+            <img
+                style={{ width: "100%" }}
+                src={pictureURL}
+                alt=""
+            />
+            <h1 style={{ margin: "0" }}>{productName}</h1>
+            <p style={{ margin: "0" }}>{nickname}</p>
+            <div>
+                <p>{discountPrice}</p>
+                <p>{price}</p>
+            </div>
         </div>
     )
 }
