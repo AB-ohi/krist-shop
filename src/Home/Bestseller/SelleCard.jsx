4789a80ba7fef76a0587929310f81931d44aec94
ABohi <askohi63907@gmail.com>
2024-02-18 01:33:44 +0600

    added

--- src/Home/Bestseller/SelleCard.jsx
+++ src/Home/Bestseller/SelleCard.jsx
@@ -8,16 +8,19 @@ const SelleCard = ({pictureURL,productName,nickname,price}) => {
 
     return (
         <div className="product-cart">
+            <div style={{position:'relative'}}>
+                <p style={{zIndex:'10', position:'absolute',backgroundColor:'red', color:'white', padding:'5px 8px'}}>30%</p>
             <img
-                style={{ width: "100%" }}
+                style={{ width: "100%", zIndex:'1' }}
                 src={pictureURL}
                 alt=""
             />
+            </div>
             <h1 style={{ margin: "0" }}>{productName}</h1>
             <p style={{ margin: "0" }}>{nickname}</p>
             <div>
-                <p>{discountPrice}</p>
-                <p>{price}</p>
+                <p>Discount price: {discountPrice}</p>
+                <p>Price: <span style={{color:'rgb(196, 196, 196)'}}>{price}</span></p>
             </div>
         </div>
     )
