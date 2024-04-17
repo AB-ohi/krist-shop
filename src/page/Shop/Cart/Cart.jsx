755fb019ab0c2dc15d7658e9e172b572ead805eb
ABohi <askohi63907@gmail.com>
2024-04-18 00:46:13 +0600

    added

--- src/page/Shop/Cart/Cart.jsx
+++ src/page/Shop/Cart/Cart.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 
 const Cart = () => {
+    const [products, setProduct] = useState([])
+
+    useEffect(()=> {
+        fetch('http://localhost:5000/men')
+        .then(res => res.json())
+        .then(data => setProduct(data))
+    },[])
     return (
         <div>
             <p>Showing 1-16 of 71 results</p>
             {/* cart section */}
+            {
+                products.map((products) => (
+                    <div key={products._id}>
 
-            
+                    </div>
+                ))
+            }
+
+
             
         </div>
     );
