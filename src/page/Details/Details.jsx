e2ce6a9d003f668944bd5ee1fd52915363c7445f
ABohi <askohi63907@gmail.com>
2024-08-04 02:06:59 +0600

    added

--- src/page/Details/Details.jsx
+++ src/page/Details/Details.jsx
@@ -1,13 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
+import './Details.css'
 
 const Details = () => {
     const detail = useLoaderData()
-    console.log(detail)
+    console.log(detail.category)
 
     return (
-        <div>
-            this is details page
-            id {detail._id}
+        <div className="detailMain">
+            <div style={{display:'flex', alignItems:'center', gap:'7px'}}>
+                <Link className="linkDeration" to='/' rel="stylesheet">home</Link>
+                <samp>{">"}</samp>
+                <Link className="linkDeration" to='/shop' rel="stylesheet" >shop</Link>
+                <samp>{">"}</samp>
+                <p style={{fontSize:'20px'}}>{detail.category}</p>
+            </div>
         </div>
     );
 };
