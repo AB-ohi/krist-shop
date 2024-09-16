35f5f31e7c33035715f90e53950cb858c772884f
ABohi <askohi63907@gmail.com>
2024-09-16 21:25:17 +0600

    added

--- src/Layout/Profile.jsx
+++ src/Layout/Profile.jsx
@@ -1,21 +1,33 @@
 import { Outlet } from "react-router-dom";
 import ProfileSideBer from "../Shared/ProfileSideBer/ProfileSideBer";
+import "./profile.css";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+// import profileBanner from '../../public/img/profileBanner.jpg'
 
 const Profile = () => {
+    const { user } = useContext(AuthContext);
+    console.log(user)
     return (
-        <div>
-            <div style={{display:'flex'}}>
-            <img style={{width:'350px', borderRadius:'100%'}} src="https://i.pinimg.com/originals/79/81/06/798106a8e12c2a545aa89876da8179ae.png" alt="" />
-            <div><p>Name</p>
-            <p>Gmail</p>
-            <p>Phone</p></div>
-            </div>
-            <div>
-            <ProfileSideBer/>
-            <Outlet/>
-            </div>
-        </div>
-    );
+    <div>
+     <div className="profileHeader">
+     <div className="profileBanner"></div>
+      <div className="profileCommonInfo">
+        <img
+        className="profilePicture"
+          style={{ width: "250px", height: "250px", borderRadius: "100%", boxShadow:'rgb(38, 57, 77) 0px 20px 30px -10px'}}
+          src="https://pics.craiyon.com/2023-11-26/oMNPpACzTtO5OVERUZwh3Q.webp"
+          alt=""
+        />
+        <h1 style={{margin:'0'}}>Name</h1>
+      </div>
+     </div>
+      <div className="profileBody">
+        <ProfileSideBer />
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
--- src/Shared/ProfileSideBer/ProfileSideBer.jsx
+++ src/Shared/ProfileSideBer/ProfileSideBer.jsx
@@ -4,9 +4,11 @@ const ProfileSideBer = () => {
     return (
 
             <div className='MainProfileSideBer'>
+            <div className='MainChild'>
             <button><Link to='#'>Profile</Link></button>
             <button><Link to='#'>Address book</Link></button>
             <button><Link to='#'>My orders</Link></button>
+            </div>
         </div>
 
     );
