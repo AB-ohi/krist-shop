edfa9601157ded86a552b149adaea658d890eb76
ABohi <askohi63907@gmail.com>
2024-09-21 02:44:02 +0600

    added

--- src/page/profile/profileInfo.jsx
+++ src/page/profile/profileInfo.jsx
@@ -1,19 +1,21 @@
 import { useLoaderData } from "react-router-dom";
-import profileInfoBG from "../../../public/img/profileInfoBG.png"
-import './ProfileInfo.css'
+import profileInfoBG from "../../../public/img/profileInfoBG.png";
+import "./ProfileInfo.css";
 
 const ProfileInfo = () => {
-    const user = useLoaderData()
-    // console.log(user)
-    return (
-        <div style={{position:'relative', width:'100%'}}>
-            <img className="profileImg" src={profileInfoBG} alt="" />
-            <p>Name: {user.displayName}</p>
-            <p>Phone:</p>
-            <p>E-mail: {user.email}</p>
-            <p>Address:</p>
-        </div>
-    );
+  const user = useLoaderData();
+  // console.log(user)
+  return (
+    <div style={{ position: "relative", width: "100%" }}>
+      <img className="profileImg" src={profileInfoBG} alt="" />
+      <div className="InfoBody">
+        <p>Name: {user.displayName}</p>
+        <p>Phone:</p>
+        <p>E-mail: {user.email}</p>
+        <p>Address:</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileInfo;
