bf74f655acd306fda6d889dacdc37352d6641a52
AB-Ohi <askohi63907@gmail.com>
2025-05-01 23:38:33 +0600

    change some style in header section

--- src/Home/Header/Header.jsx
+++ src/Home/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <p style={{margin:'0 0 10px 0'}}>Classic Exclusive</p>
                 {/* eslint-disable-next-line react/no-unescaped-entities */}
                 <h1 style={{fontSize:'40px', fontWeight:'800', fontFamily:" 'Jost', sans-serif", marginTop:'0'}}>Woman's Collection</h1>
-                <button className='header-Btn'><Link style={{textDecoration:'none',color:'white'}} to='/shop'>Show More</Link></button>
+                <Link className='header-Btn'to='/shop'>Show More</Link>
             </div>
            
                 <img className='banner-pic' src="../../../public/img/homeBanner.png" alt="" />
--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -8,7 +8,7 @@ import Header from "../Header/Header";
 const Home = () => {
     return (
         <div>
-            <Header/>
+            <Header />
             <Categories/>
             <Bestseller/>
             <Events/>
