3b86e36b8be2c69b7c11c5f70933043c175ef1ad
AB-Ohi <askohi63907@gmail.com>
2025-05-06 11:54:51 +0600

    setup manage user page and style this page

--- src/page/Admin/ManageUser.jsx
+++ src/page/Admin/ManageUser.jsx
@@ -1,34 +1,44 @@
-import useUserData from "../../Hook/useUserData"
+import useUserData from "../../Hook/useUserData";
+import './ManageUser.css'
 
-const ManageUser = () =>{
-    const {AllUser}= useUserData()
-    console.log(AllUser)
-    
-    return(
-        <div style={{marginLeft:'20px'}}>
-            <h1>User List</h1>
-            <table>
-                <tr>
-                    <th>User NO</th>
-                    <th>User Name</th>
-                    <th>User Mail</th>
-                    <th>User Phone</th>
-                    <th>User Id</th>
-                    <th>Role</th>
-                    <th>Actions</th>
-                </tr>
-                <tbody>
-                    {
-                        AllUser.map((allUser)=>(
-                                <tr key={allUser._id}>
+const ManageUser = () => {
+  const { AllUser } = useUserData();
+  console.log(AllUser);
 
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
-
-        </div>
-    )
-}
-export default ManageUser
+  return (
+    <div style={{ marginLeft: "20px" }}>
+      <h1>User List</h1>
+      <div style={{overflowX:'scroll'}}>
+      <table className="manage_uaer_table_body">
+        <tr className="ManageUser_table_header">
+          <th>User NO</th>
+          <th>User Name</th>
+          <th>User Mail</th>
+          <th>User Phone</th>
+          <th>Role</th>
+          <th>Actions</th>
+        </tr>
+        <tbody>
+          {AllUser.map((allUser, index) => (
+            <tr className="manage_user_table_data" style={{textAlign:'center'}} key={allUser._id}>
+                <td>{index+1}</td>
+                <td>{allUser.profileIDName}</td>
+                <td>{allUser.email}</td>
+                <td>{allUser.phoneNumber}</td>
+                <td>{allUser.role}</td>
+                {
+                    allUser?.role == 'admin'?(
+                        <td style={{borderRight:'2px solid #6A42C7'}}><button className="role_change_btn">castomar</button></td>
+                    ):(
+                        <td style={{borderRight:'2px solid #6A42C7'}}><button className="role_change_btn">admin</button></td>
+                    )
+                }
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    </div>
+  );
+};
+export default ManageUser;
