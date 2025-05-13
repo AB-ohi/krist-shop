f8c068cf768025caeeab671309af0764c2a97a02
AB-Ohi <askohi63907@gmail.com>
2025-05-13 12:09:30 +0600

    added the uplode product section

--- src/page/Admin/manageProduct.jsx
+++ src/page/Admin/manageProduct.jsx
@@ -1,8 +1,63 @@
-const ManageProduct = () =>{
-    return(
-        <div>
+import React from "react";
+import "./manageProduct.css";
+import ImageUploading from "react-images-uploading";
+const ManageProduct = () => {
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 69;
 
-        </div>
-    )
-}
-export default ManageProduct
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
+  return (
+    <div className="manage_product_body">
+      <h1>Upload product</h1>
+      <div>
+        <ImageUploading
+          multiple
+          value={images}
+          onChange={onChange}
+          maxNumber={maxNumber}
+          dataURLKey="data_url"
+        >
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            // write your building UI
+            <div className="upload__image-wrapper">
+              <button
+                style={isDragging ? { color: "red" } : undefined}
+                className="imgSelectBtn"
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Select your product
+              </button>
+              
+              <div className="imgArea">
+              {imageList.map((image, index) => (
+                <div key={index} className="image-item">
+                  <img style={{width:'240px'}} src={image["data_url"]} alt="" width="100" />
+                  <div className="image-item__btn-wrapper">
+                    <button onClick={() => onImageRemove(index)}>Remove</button>
+                  </div>
+                </div>
+              ))}
+              </div>
+              <button onClick={onImageRemoveAll}>Remove all images</button>
+            </div>
+          )}
+        </ImageUploading>
+      </div>
+    </div>
+  );
+};
+export default ManageProduct;
