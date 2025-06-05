20b70cb57d38da59adcbe01e6e305d3433655356
AB-Ohi <askohi63907@gmail.com>
2025-06-06 01:27:22 +0600

    added

--- src/component/manageProductAdmin.jsx
+++ src/component/manageProductAdmin.jsx
@@ -1,19 +1,46 @@
-import allProductHook from "../Hook/allProductHook"
+import allProductHook from "../Hook/allProductHook";
 
-const ManageProductAdmin=()=>{
-    const allProducts = allProductHook()
-    console.log(allProducts)
-    return(
-        <div>
-             {
-                allProducts?.map((allProduct) =>{
-                   return ( <div key={allProduct._id}>
-                        <img src={allProduct.images?.[0]} alt="" />
-                    </div>)
-                })
-             }
-        </div>
-    )
-}
+const ManageProductAdmin = () => {
+  const allProducts = allProductHook();
+  console.log(allProducts);
+  return (
+    <div>
+      <table>
+        <tr>
+          <th></th>
+          <th>User Name</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Role</th>
+          <th>Actions</th>
+        </tr>
+        <tbody>
+          {allProducts?.map((allProduct) => {
+            return (
+            
+                <tr style={{ display: "flex", alignItems: "center", gap: "10px" }}
+                key={allProduct._id}>
+                 <td>
+                     <img
+                    style={{ width: "50px" }}
+                    src={allProduct.images?.[0]}
+                    alt=""
+                  />
+                 </td>
+               
+                <td>
+                  <p>{allProduct.product_name}</p>
+                </td>
+                <td>
+                    {allProduct.main_price}
+                </td>
+            </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default ManageProductAdmin;
