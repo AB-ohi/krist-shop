d66950358163396860c3e8e3bcd74990ce89c467
AB-Ohi <askohi63907@gmail.com>
2025-06-06 01:34:29 +0600

    added

--- src/component/manageProductAdmin.jsx
+++ src/component/manageProductAdmin.jsx
@@ -6,35 +6,33 @@ const ManageProductAdmin = () => {
   return (
     <div>
       <table>
-        <tr>
-          <th></th>
-          <th>User Name</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Role</th>
-          <th>Actions</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
         <tbody>
           {allProducts?.map((allProduct) => {
             return (
-            
-                <tr style={{ display: "flex", alignItems: "center", gap: "10px" }}
-                key={allProduct._id}>
-                 <td>
-                     <img
+              <tr style={{backgroundColor:'pink'}} key={allProduct._id}>
+                <td>
+                  <img
                     style={{ width: "50px" }}
                     src={allProduct.images?.[0]}
                     alt=""
                   />
-                 </td>
-               
+                </td>
+
                 <td>
                   <p>{allProduct.product_name}</p>
                 </td>
-                <td>
-                    {allProduct.main_price}
-                </td>
-            </tr>
+                <td>{allProduct.main_price}</td>
+              </tr>
             );
           })}
         </tbody>
