fc82f5c1ebec4e4f2bacb7256308c60ed0e7c529
AB-Ohi <askohi63907@gmail.com>
2025-06-12 02:56:29 +0600

    update prodeact section

--- src/component/updateProduct.jsx
+++ src/component/updateProduct.jsx
@@ -1,44 +1,56 @@
-import { useState } from 'react';
-import './updateProduct.css'
-const UpdateProduct = ( {editProduct, setEditProduct}) => {
+import { useState } from "react";
+import "./updateProduct.css";
+const UpdateProduct = ({ editProduct, setEditProduct }) => {
   if (!editProduct) {
-    return null; 
+    return null;
   }
 
-  console.log("producr",editProduct?._id)
+  console.log("product", editProduct?._id);
   return (
     <div className="update_product_form">
       <h2>Update Product: {editProduct?.product_name}</h2>
       <p>Id: {editProduct?._id}</p>
-      <form className='update_product_from'>
+      <form className="update_product_from">
         <label>
           Product Name:
-          <input type="text" defaultValue={editProduct.product_name} />
+          <input type="text" placeholder={editProduct.product_name} />
         </label>
         <br />
         <label>
           Price:
-          <input type="number" defaultValue={editProduct.main_price} />
+          <input
+              onInput={(e) => {
+                        e.target.value = e.target.value.replace(/[^0-9.]/g, "");
+                      }}
+            type="text"
+            placeholder={editProduct.main_price}
+          />
         </label>
         <br />
         <label>
           Quantity:
-          <input type="number" defaultValue={editProduct.quantity} />
+          <input
+           onInput={(e) => {
+                        e.target.value = e.target.value.replace(/[^0-9.]/g, "");
+                      }}
+            type="text"
+            placeholder={editProduct.quantity}
+          />
         </label>
         <br />
         <label>
           Discount:
-          <input type="number" defaultValue={editProduct.discount} />
+          <input
+            onInput={(e) => {
+              e.target.value = e.target.value.replace(/[^0-9.]/g, "");
+            }}
+            type="text"
+            placeholder={editProduct.discount}
+          />
         </label>
-        <br />
-        <label>
-          Discount Price:
-          <input type="number" defaultValue={editProduct.discount_price} />
-        </label>
-        <br />
       </form>
-        <button type="submit">Update</button>
-        <button onClick={()=>setEditProduct(null)}>cancel</button>
+      <button type="submit">Update</button>
+      <button onClick={() => setEditProduct(null)}>cancel</button>
     </div>
   );
 };
