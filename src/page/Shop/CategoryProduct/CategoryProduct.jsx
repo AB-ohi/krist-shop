6dd3b5fd9b5f4dac3098e13e53d807f802b202c1
AB-Ohi <askohi63907@gmail.com>
2025-07-06 01:51:16 +0600

    added

--- src/page/Shop/CategoryProduct/CategoryProduct.jsx
+++ src/page/Shop/CategoryProduct/CategoryProduct.jsx
@@ -16,7 +16,19 @@ const CategoryProduct = () =>{
                                     <img style={{width:'100%'}} src={product.images[0]} alt="" />
                                 </div>
                                 <div>
-                            <p>{product.product_detail}</p>
+                                <h2 style={{fontSize:'15px'}}>{product.product_name}</h2>
+
+                                {
+                                    product?.discount > 0 ?(
+                                        <div>
+                                            <p>{product.main_price} ৳</p>
+                                            <p>{product.discount}%</p>
+                                            <p>{product.discount_price}</p>
+                                        </div>
+                                    ):(
+                                        <div></div>
+                                    )
+                                }
                                 </div>
                             </div>
                         )
