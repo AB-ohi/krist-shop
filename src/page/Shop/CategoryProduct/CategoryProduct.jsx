dc55e19cae2748a9ae3dda316547386cf9e252f5
AB-Ohi <askohi63907@gmail.com>
2025-07-06 02:40:08 +0600

    update product css

--- src/page/Shop/CategoryProduct/CategoryProduct.jsx
+++ src/page/Shop/CategoryProduct/CategoryProduct.jsx
@@ -29,35 +29,43 @@ const CategoryProduct = () => {
         }}
       >
         {products?.map((product) => (
-         <div className="shopDisplayProductCard" key={product._id}>
-  <div className="shopDisplayProductImgWrapper">
-    <img src={product.images?.[0]} alt={product.product_name} />
-    <div className="imgOverlay">
-      <button className="detailsButton">Details</button>
-    </div>
-  </div>
+          <div className="shopDisplayProductCard" key={product._id}>
+            <div className="shopDisplayProductImgWrapper">
+              <img src={product.images?.[0]} alt={product.product_name} />
+              <div className="imgOverlay">
+                <button className="detailsButton">Details</button>
+              </div>
+            </div>
 
-  <div style={{ marginTop: "10px" }}>
-    <h2 style={{ fontSize: "15px", fontWeight: "600" }}>{product.product_name}</h2>
+            <div style={{ margin: "10px 0 10px 10px" }}>
+              <h2 style={{ fontSize: "15px", fontWeight: "600", color: "" }}>
+                {product.product_name}
+              </h2>
 
-    {product?.discount > 0 ? (
-      <div style={{ marginTop: "5px", fontSize: "14px" }}>
-        <p style={{ textDecoration: "line-through", color: "gray" }}>
-          Original Price: ৳{product.main_price}
-        </p>
-        <p style={{ color: "green", fontWeight: "bold" }}>
-          Discounted Price: ৳{product.discount_price}
-        </p>
-        <p style={{ color: "red" }}>Discount: {product.discount}%</p>
-      </div>
-    ) : (
-      <p style={{ marginTop: "5px", fontWeight: "bold", color: "green" }}>
-        ৳{product.main_price}
-      </p>
-    )}
-  </div>
-</div>
-
+              {product?.discount > 0 ? (
+                <div style={{ marginTop: "5px", fontSize: "14px", position:"relative" }}>
+                  <p style={{ textDecoration: "line-through", color: "gray" }}>
+                    Original Price: ৳{product.main_price}
+                  </p>
+                  <p style={{ color: "#007180", fontWeight: "bold" }}>
+                    Discounted Price: ৳{product.discount_price}
+                  </p>
+                  <p className="discountInProduct">{product.discount}%</p>
+                  
+                </div>
+              ) : (
+                <p
+                  style={{
+                    marginTop: "5px",
+                    color: "#007180",
+                    fontWeight: "bold",
+                  }}
+                  >
+                  Price: ৳{product.main_price}
+                </p>
+              )}
+            </div>
+          </div>
         ))}
       </div>
     </div>
