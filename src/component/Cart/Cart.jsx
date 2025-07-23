82cf7c3db7337d19c6fa50aa81eba443b5d6cb9d
AB-Ohi <askohi63907@gmail.com>
2025-07-23 18:24:53 +0600

    add the all icon

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,50 +1,58 @@
 import React from 'react'
 import cartHook from '../../Hook/cartHook'
-import { FcManager,FcBusinesswoman } from "react-icons/fc";
+import { FcManager, FcBusinesswoman } from "react-icons/fc";
 import { LuFootprints } from "react-icons/lu";
 import { MdOutlineChildCare } from "react-icons/md";
-import { GiWesternHat } from "react-icons/gi";
+import { GiWesternHat, GiFireworkRocket } from "react-icons/gi";
+import "./cart.css"
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-    const [cart, setCurt] = cartHook()
-    const categories = [
+    const [cart, setCart] = cartHook();
+
+  const categories = [
     {
       category: "Men",
-      Logo: <FcManager/>,
+      Logo: <FcManager />,
       url: "/shop/man",
     },
     {
       category: "Women",
-      Logo: <FcBusinesswoman/>,
+      Logo: <FcBusinesswoman />,
       url: "/shop/women",
     },
     {
       category: "Foot",
-      Logo: <LuFootprints/>,
+      Logo: <LuFootprints />,
       url: "/shop/foot",
     },
     {
       category: "Kids",
-      Logo: <MdOutlineChildCare/>,
+      Logo: <MdOutlineChildCare />,
       url: "/shop/kids",
     },
     {
       category: "B&F",
-      Logo: "",
+      Logo: <GiFireworkRocket />,
       url: "/shop/bangla",
     },
     {
       category: "Western",
-      Logo: <GiWesternHat/>,
+      Logo: <GiWesternHat />,
       url: "/shop/western",
     },
   ];
+
   return (
-    <div>
+    <div className='carts'>
       {
        categories?. map((category,index) =>(
-        <div key={index}>
-            dcdcd
+        <div className='cart' key={index}>
+           <Link className='cartLink' to={category.url}>
+           <p>{category.Logo}</p>
+           <p>{category.category}</p>
+           </Link>
+
         </div>
        )) 
       }
