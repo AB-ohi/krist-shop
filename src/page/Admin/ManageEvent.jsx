e666c19f59f6e6b765a5df8fad12a373245a45a8
AB-Ohi <askohi63907@gmail.com>
2025-08-14 00:09:11 +0600

    update the image add style

--- src/page/Admin/ManageEvent.jsx
+++ src/page/Admin/ManageEvent.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import ImageUploading from "react-images-uploading";
-import event from "../../../public/img/event.png"
-import './ManageEvent.css'
+import event from "../../../public/img/event.png";
+import "./ManageEvent.css";
 import { RxCross2 } from "react-icons/rx";
 
 const ManageEvent = () => {
-    const [images, setImages] = useState([]);
-      const onChange = (imageList, addUpdateIndex) => {
+  const [images, setImages] = useState([]);
+  const onChange = (imageList, addUpdateIndex) => {
     console.log(imageList, addUpdateIndex);
     setImages(imageList);
   };
 
-   const maxNumber = 69;
+  const maxNumber = 69;
 
   return (
     <div className="evenMain">
-        <h1>Manage Event</h1>
+      <h1>Manage Event</h1>
       <ImageUploading
         multiple
         value={images}
@@ -32,38 +32,41 @@ const ManageEvent = () => {
           isDragging,
           dragProps,
         }) => (
-          <div className="upload__image-wrapper">
+          <div className={images?.length > 0 ? "upload__image-wrapper hasImages" : "upload__image-wrapper"}>
             <button
-            className="picAddBtn"
+              className="picAddBtn"
               onClick={onImageUpload}
               {...dragProps}
             >
               Add Event Picture
             </button>
             {imageList.map((image, index) => (
-                <div key={index} className="even_image-item">
-                <img src={image['data_url']} alt=""  width="100%" />
+              <div key={index} className="even_image-item">
+                <img src={image["data_url"]} alt="" />
                 <div className="event-image-item__btn-wrapper">
-                  <button onClick={() => onImageRemove(index)}><RxCross2 style={{ paddingTop: "5px" }} /></button>
+                  <button onClick={() => onImageRemove(index)}>
+                    <RxCross2 style={{ paddingTop: "5px" }} />
+                  </button>
                 </div>
               </div>
             ))}
 
-            {
-                images?.length> 0?(
-                <div>
-                    <button className="removeEventPicBtn" onClick={onImageRemoveAll}>Remove all images</button>
-                    <div>
-
-                    </div>
-                </div>
-            ):
-                (<div className="emptyEvent">
-                    <img src={event} alt="" />
-                    <div></div>
-                </div>)
-            }
-            
+            {images?.length > 0 ? (
+              <div>
+                <button
+                  className="removeEventPicBtn"
+                  onClick={onImageRemoveAll}
+                >
+                  Remove all images
+                </button>
+                <div></div>
+              </div>
+            ) : (
+              <div className="emptyEvent">
+                <img src={event} alt="" />
+                <div></div>
+              </div>
+            )}
           </div>
         )}
       </ImageUploading>
