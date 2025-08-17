7bf68e80cb19cf30fc1ca4c7200260803c17bdf2
AB-Ohi <askohi63907@gmail.com>
2025-08-17 23:55:30 +0600

    add input

--- src/page/Admin/ManageEvent.jsx
+++ src/page/Admin/ManageEvent.jsx
@@ -3,9 +3,9 @@ import ImageUploading from "react-images-uploading";
 import event from "../../../public/img/event.png";
 import "./ManageEvent.css";
 import { RxCross2 } from "react-icons/rx";
-
 const ManageEvent = () => {
   const [images, setImages] = useState([]);
+  const [eventData, setEvenData] = useState();
   const onChange = (imageList, addUpdateIndex) => {
     console.log(imageList, addUpdateIndex);
     setImages(imageList);
@@ -18,88 +18,116 @@ const ManageEvent = () => {
       <h1>Manage Event</h1>
       <form action="">
         <ImageUploading
-        multiple
-        value={images}
-        onChange={onChange}
-        maxNumber={maxNumber}
-        dataURLKey="data_url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          onImageRemoveAll,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-        }) => (
-          <div className={images?.length > 0 ? "upload__image-wrapper hasImages" : "upload__image-wrapper"}>
-            <p
-              className={images?.length>0?"againAdd":"picAddBtn"}
-              onClick={onImageUpload}
-              {...dragProps}
+          multiple
+          value={images}
+          onChange={onChange}
+          maxNumber={maxNumber}
+          dataURLKey="data_url"
+        >
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            <div
+              className={
+                images?.length > 0
+                  ? "upload__image-wrapper hasImages"
+                  : "upload__image-wrapper"
+              }
             >
-              Add Event Picture
-            </p>
-            {imageList.map((image, index) => (
-              <div key={index} className="even_image-item">
-                <img src={image["data_url"]} alt="" />
-                <div className="event-image-item__btn-wrapper">
-                  <p onClick={() => onImageRemove(index)}>
-                    <RxCross2 style={{ paddingTop: "5px" }} />
-                  </p>
+              <p
+                className={images?.length > 0 ? "againAdd" : "picAddBtn"}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Add Event Picture
+              </p>
+              {imageList.map((image, index) => (
+                <div key={index} className="even_image-item">
+                  <img src={image["data_url"]} alt="" />
+                  <div className="event-image-item__btn-wrapper">
+                    <p onClick={() => onImageRemove(index)}>
+                      <RxCross2 style={{ paddingTop: "5px" }} />
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
 
-            {images?.length > 0 ? (
-              <div>
-                <button
-                  className="removeEventPicBtn"
-                  onClick={onImageRemoveAll}
-                >
-                  Remove all images
-                </button>
-                <div></div>
-              </div>
-            ) : (
-              <div className="emptyEvent">
-                <img src={event} alt="" />
-                <div></div>
-              </div>
-            )}
+              {images?.length > 0 ? (
+                <div>
+                  <button
+                    className="removeEventPicBtn"
+                    onClick={onImageRemoveAll}
+                  >
+                    Remove all images
+                  </button>
+                  <div></div>
+                </div>
+              ) : (
+                <div className="emptyEvent">
+                  <img src={event} alt="" />
+                  <div></div>
+                </div>
+              )}
+            </div>
+          )}
+        </ImageUploading>
+        {images?.length > 0 ? (
+          <div>
+            <h1>Event description</h1>
+            <div>
+              <label>Event title</label>
+              <input
+                type="text"
+                name="title"
+                id=""
+                onChange={(e) =>
+                  setEvenData({ ...eventData, title: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label>Event details</label>
+              <input
+                type="text"
+                name="details"
+                onChange={(e) =>
+                  setEvenData({ ...eventData, details: e.target.value })
+                }
+                id=""
+              />
+            </div>
+            <div>
+              <label>Event condition</label>
+              <input
+                type="text"
+                name="condition"
+                onChange={(e) =>
+                  setEvenData({ ...eventData, condition: e.target.value })
+                }
+                id=""
+              />
+            </div>
+            <div>
+              <label>Discount</label>
+              <input
+                type="text"
+                name="discount"
+                onChange={(e) =>
+                  setEvenData({ ...eventData, discount: e.target.value })
+                }
+                id=""
+              />
+            </div>
           </div>
+        ) : (
+          <div></div>
         )}
-      </ImageUploading>
-      {
-        images?.length> 0?(
-            <div>
-                <h1>Event description</h1>
-                <div>
-                    <label>Event title</label>
-                <input type="text" name="title" id="" />
-                </div>
-                <div>
-                    <label>Event details</label>
-                <input type="text" name="details" id="" />
-                </div>
-                <div>
-                    <label>Event condition</label>
-                <input type="text" name="condition" id="" />
-                </div>
-                <div>
-                    <label>Discount</label>
-                <input type="text" name="discount" id="" />
-                </div>
-
-            </div>
-        ):(
-            <div>
-
-            </div>
-        )
-      }
-      
       </form>
     </div>
   );
