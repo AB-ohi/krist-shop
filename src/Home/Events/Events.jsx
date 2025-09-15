b018edc0ebfc2c96fb6b0529b7168a4e34cd4a87
AB-Ohi <askohi63907@gmail.com>
2025-09-15 19:02:36 +0600

    added

--- src/Home/Events/Events.jsx
+++ src/Home/Events/Events.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import adminEventHook from "../../Hook/adminEventHook";
+import discount_BG from "../../../public/img/discount_background.png"
 
 const EventDetail = () => {
   const [adminEvent] = adminEventHook();
@@ -8,7 +9,7 @@ const EventDetail = () => {
   const totalImages = adminEvent?.[0]?.eventImage?.length || 0;
 
   useEffect(() => {
-    if (totalImages === 0) return; 
+    if (totalImages === 0) return;
     const interval = setInterval(() => {
       setCurrentImg((prev) => (prev + 1) % totalImages);
     }, 5000);
@@ -42,21 +43,24 @@ const EventDetail = () => {
         <p style={{ fontSize: "16px", marginBottom: "15px" }}>
           <strong>Condition:</strong> {event.condition}
         </p>
-        <p style={{ fontSize: "16px", marginBottom: "15px" }}>
-          <strong>Discount:</strong> {event.discount}%
-        </p>
       </div>
 
       <div style={{ flex: 1, position: "relative" }}>
+        <div>
+            <img src={discount_BG} style={{position:"absolute", width:"100px", zIndex:'0'}} alt="" />
+          <p style={{ fontSize: "16px", marginBottom: "15px", position:'absolute', zIndex:'10' }}>
+           {event.discount} <br />%
+          </p>  
+        </div>
         {totalImages > 0 && (
           <img
             src={event.eventImage[currentImg]}
             alt={event.title}
             style={{
-              width: "100%", 
-              height: "400px", 
+              width: "100%",
+              height: "400px",
               borderRadius: "8px",
-              objectFit: "cover", 
+              objectFit: "cover",
               transition: "all 0.5s ease",
             }}
           />
